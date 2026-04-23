cd912f52639c74cfef09e2c32771ff141797c03e
Mohamed Fazil <mohamedfazil152@gmail.com>
2026-04-23 21:41:56 +0530

    geerate token functions have been added

--- auth-service/src/main/java/com/pm/authservice/service/AuthService.java
+++ auth-service/src/main/java/com/pm/authservice/service/AuthService.java
@@ -2,6 +2,9 @@ package com.pm.authservice.service;
 
 import com.pm.authservice.dto.LoginRequestDTO;
 import com.pm.authservice.model.User;
+import com.pm.authservice.utils.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -9,13 +12,17 @@ import java.util.Optional;
 @Service
 public class AuthService {
     private final UserService userService;
+    @Autowired
+    private  PasswordEncoder passwordEncoder;
+    @Autowired
+    private JwtUtil jwtUtil;
     //constructor
     public AuthService(UserService userService){
         this.userService=userService;
     }
     public Optional<String> authenticate(LoginRequestDTO loginRequestDTO){
      Optional<String> token=userService.findByEmail(loginRequestDTO.getEmail())
-             .filter(u->passwordEncoder.maches(loginRequestDTO.getPassword(),u.getPassword()))
+             .filter(u->passwordEncoder.matches(loginRequestDTO.getPassword(),u.getPassword()))
              .map(u->jwtUtil.generateToken(u.getEmail(),u.getRole()));
      return token;
     }
